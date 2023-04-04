43d46d8c928b3a8ab3bc265e935a2f201ee9cc50
hafizbukhari21 <hafiz.bukhari@hotmail.com>
2023-04-04 08:46:30 +0700

    Add fitur parse bit 48

---
+++ libraryBiller.js
@@ -0,0 +1,52 @@
+/*
+Code = 10000002001-Res
+PLN Prepaid Advice
+210 Payment Response
+230 Payment Advice Response
+*/
+
+let biller_lib = [
+     {
+        /*
+        PLN Prepaid Advice
+        210 Payment Response
+        230 Payment Advice Response
+        */
+        code:"10000002001-Res",
+        title:"Biller - PLN Prepaid: 210 Payment Response, 230 Advice Response",
+        expression:[
+            "Meter ID / Customer ID|20",
+            "ID Code|1",
+            "Buying Options|1",
+            "Date|8",
+            "Time|6",
+            "Customer Name|30",
+            "Buy Amount|12",
+            "Admin Fee|12",
+            "Reference|32",
+            "Customer|32",
+            "Reff|10",
+            "Meter Serial Number|15",
+            "Admin Fee / Admin CA|16",
+            "Customer Name|25",
+            "Materai|16",
+            "Tarif / Daya|17",
+            "PPN|16",
+            "Reff|32",
+            "PPJ|16",
+            "Angsuran|16",
+            "Rp. Bayar|16",
+            "Rp. Token|16",
+            "Kwh|12",
+            "Token Number|20",
+            "Rp Token / KWH|16",
+            "No Telp Rayon|15",
+            "Reserved 1|32",
+            "Reserved 2|32",
+            "Label Ref|10",
+            "Biller Message|74",
+        ]
+        }
+
+        
+]
--- parse.js
+++ parse.js
@@ -1,17 +1,45 @@
 
 
-function ParseThis(input, titleData,separator){
+function ParseThis(input, titleData,separator,isBiller){
     let output = document.querySelector(".output")
     if (input.value.length ==0) output.style.display="none"
     else output.style.display="inline"
+    // console.log(input.value)
 
-    SpliteString(input,output)
+    SpliteString(input,output,separator,isBiller)
 
-    function SpliteString(input,output){
-        let arrData = input.value.split(separator)
+
+
+    function SpliteString(input,output,separator, isBiller){
+        console.log(isBiller)
+        let arrData = []
+        if(!isBiller) arrData=input.value.split(separator)
+        else arrData=ParseBiller(input,titleData)
         ShowData( output, arrData)
-        console.log(titleData)
 
+    }
+
+    //kalo dia biller
+    function ParseBiller(inputBiller,titleData){
+        let start=0
+        let end=0
+        let currentMax =0
+        let tempArr = []
+        titleData.forEach((e,idx,arr)=>{
+            
+            let ambilSebanyak = parseInt(e.split("|")[1])
+            //console.log(ambilSebanyak)
+            //console.log(tempArr)
+            end=end+ambilSebanyak
+            tempArr.push(inputBiller.value.substring(start,end))
+            console.log(tempArr)
+            start=start+ambilSebanyak
+            
+            
+        })
+
+
+        return tempArr
     }
     
     function ShowData( output,arrData){
